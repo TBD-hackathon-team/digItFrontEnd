ebc2614b8afe10db9e8631377acd9c22c6bb4bb1
mia <mia5@Umbc.edu>
2017-10-27 17:23:16 -0400

    just barely added a little more...

--- digIt-app/src/index.js
+++ digIt-app/src/index.js
@@ -1,21 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import axios from 'axios';
 
 class BitcoinTile extends React.Component{
     render(){
         return(
-            <div className="bitcoinTile">
-            <p> I am a test! Eventually I will have bitcoin information...</p>
-            </div> 
+                <div className="bitCoinTile">
+                    <label className="bitCoinPriceLabel">Bitcoin price: </label>
+                    <br /> 
+                    <span className="bitCoinPrice">Current bitcoin price: </span>
+                </div>
+
         );
     } 
     
 }
 
+class BitCoinRestCall extends React.Component {
+    constructor() {
+        super();
+    
+        this.state = {
+          posts: []
+        };
+      }
+    
+      componentDidMount() {
+        axios.get(`http://globalcurrencies.xignite.com/xGlobalCurrencies.json/GetHistoricalRate?_token=AE4A02E0271A4E77B78B314AEE9A132D&Symbol=EURUSD&PriceType=Mid&AsOfDate=10/20/2017&FixingTime=22:00`)
+          .then(res => {
+            const posts = res.data.data.children.map(obj => obj.data);
+            this.setState({ posts });
+          });
+      }
+    
+      render() {
+        return (
+          <div>
+            <ul>
+              {this.state.posts}
+            </ul>
+          </div>
+        );
+      }
+    }
+   
+
 
 
 ReactDOM.render(
-  <BitcoinTile />,
+  <BitCoinRestCall />,
   document.getElementById('root')
 );
